d64c8c3ba669abe2b11a9e1b05d12bd4d1d47d81
MPeggy <gypeg426@gmail.com>
2025-01-08 10:48:29 +0100

    MAJ lien

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,6 +6,7 @@ export const allRoutes = [
     new Route("/coaching", "Coaching", "/pages/coaching.html"),
     new Route("/equicoaching", "Equicoaching", "/pages/equicoaching.html"),
     new Route("/relation", "Relation", "/pages/relation.html"),
+    new Route("/jesuis", "Qui je suis", "/pages/jesuis.html"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html",["disconnected"], "js/auth/signin.js"),
     new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html", ["client", "admin"]),
